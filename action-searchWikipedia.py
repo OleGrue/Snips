05f0c338e906b712c4229e29881a148739094532
OleGrue <49246995+OleGrue@users.noreply.github.com>
2019-05-14 17:00:11 +0200

    Removed unnecesary code (config, ...)

--- action-searchWikipedia.py
+++ action-searchWikipedia.py
@@ -5,43 +5,19 @@ import re
 import io
 import sys
 
-import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import wikipedia as wiki
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
-
-
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
 
 
 def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+    action_wrapper(hermes, intentMessage)
 
 
-def action_wrapper(hermes, intentMessage, conf):
-    """
+def action_wrapper(hermes, intentMessage):
 
-    :param hermes:
-    :param intentMessage:
-    :param conf:
-    :return:
-    """
     if len(intentMessage.slots.article_indicator) > 0:
         article = intentMessage.slots.article_indicator.first().value
         wiki.set_lang('de')
